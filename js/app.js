165f241273e4366e89e83e21e0410fe6226e9b44
S-Mason <S-Mason@knights.ucf.edu>
2015-05-31 23:47:34 -0400

    modified so when hamburger icon is clicked in smaller viewports the navigation will slide down

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
+$(document).ready(function()
+{
+	$(".fa-bars").click(function() 
+	{
+		$(".nav_list").slideToggle();
+	
+	}); 
 
+
+
+}); 
 // http://mayflowerscha.com/wp-content/uploads/2014/11/sweetpeas-bouquet-m.jpg
+
+// http://theversed.com/wp-content/uploads/2013/01/Beyonce2.jpg
