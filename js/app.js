0fbf741c360cfb77c0541858ee6ddca2bdf4e9b5
S-Mason <S-Mason@knights.ucf.edu>
2015-06-03 20:46:06 -0400

    Added a smooth scrolling effect

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 $(document).ready(function()
 {
+	// Navigation - dropdown menu 
 	$(".fa-bars").click(function() 
 	{
 		$(".nav_list").slideToggle();
-	
+		return false;
 	}); 
-
-
+	
+	// Smooth scrolling
+	var $root = $('html, body'); 
+	$('a').click(function()
+	{
+		$root.animate({
+			scrollTop: $($.attr(this, 'href')).offset().top
+			}, 900);
+			return false;
+			
+	
+	});
 
 }); 
 // http://mayflowerscha.com/wp-content/uploads/2014/11/sweetpeas-bouquet-m.jpg
